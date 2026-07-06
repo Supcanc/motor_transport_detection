8ee8ec0466b0962983f408201aad9e053280dace
Ilya <i.v.nidelko@gmail.com>
2026-07-06 17:59:31 +0500

    Added annots downloading

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import requests
 from pathlib import Path
+import os
+import shutil
 
 BASE_URL = 'http://127.0.0.1:8000'
 
@@ -14,11 +16,19 @@ model_version = st.selectbox(
     ]
 )
 
+if "uploader_key" not in st.session_state:
+    st.session_state.uploader_key = 0
+
 uploaded_files = st.file_uploader(
     'Choose a directory that contains all the images you want to make prediction of',
     type=['image/*'],
-    accept_multiple_files='directory'
+    accept_multiple_files='directory',
+    key=f"uploader_{st.session_state.uploader_key}"
 )
+
+if st.button("Clear uploader"):
+    st.session_state.uploader_key += 1
+    st.rerun()
 
 if st.button('Make prediction') and uploaded_files:
     files = []
@@ -42,9 +52,31 @@ if st.button('Make prediction') and uploaded_files:
     )
 
     if prediction_response.status_code == 200:
-        for pred in prediction_response.json()['results'].keys():
-            file_name = Path(pred).name
-            st.image(pred, caption=file_name)
+        preds = prediction_response.json()['results']
+
+        annots_dir_path = Path('annots')
+        annots_dir_path.mkdir(parents=True, exist_ok=True)
+
+        for file_path in preds.keys():
+            file_name = Path(file_path).name
+            file_stem = Path(file_path).stem
+            with open(os.path.join(annots_dir_path, file_stem + '.txt'), 'w') as f:
+                f.write('x y x y confidence class_id\n')
+                for obj in preds[file_path]:
+                    f.write(f'{" ".join(str(item) for item in obj)}\n')
+
+        shutil.make_archive('annots', 'zip', annots_dir_path)
+        with open('annots.zip', 'rb') as f:
+            st.download_button(
+                label="Download all annotations",
+                data=f,
+                file_name="annots.zip",
+                mime="application/zip",
+            )
+
+        for file_path in preds.keys():
+            file_name = Path(file_path).name
+            st.image(file_path, caption=file_name)
     else:
         st.error(f'Error while making prediction:\n{prediction_response.json().get('detail')}')
 
